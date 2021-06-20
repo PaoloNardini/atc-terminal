eac2857f1cab6cb1e95f376df6543f2a667ba128
Paolo Nardini <paolo.nardini@bravosconto.it>
2021-06-20 21:34:06 +0200

    wip

--- src/core/entities/Navaid.ts
+++ src/core/entities/Navaid.ts
@@ -1,3 +1,41 @@
+import { Coordinate, Level, Speed } from "../valueObjects";
+
+export enum NavaidType {
+    NAVAID_TYPE_VORDME = 1,
+    NAVAID_TYPE_VOR = 2,
+    NAVAID_TYPE_NDB = 3,
+    NAVAID_TYPE_VORDMENDB = 4,
+    NAVAID_TYPE_TACAN = 5,
+    NAVAID_TYPE_ILS_CAT_1 = 6,
+    NAVAID_TYPE_ILS_CAT_2 = 7,
+    NAVAID_TYPE_ILS_CAT_3 = 8,
+    NAVAID_TYPE_ILS_OM = 9,
+    NAVAID_TYPE_ILS_MM = 10,
+    NAVAID_TYPE_ILS_IM = 11,
+    NAVAID_TYPE_RWY = 12,
+    NAVAID_TYPE_ROUTE_FIX = 13,
+}
 export interface Navaid {
-    
+    // General fix data
+    name: string         // Fix name
+    label: string        // Label on the screen
+    type: NavaidType          // See const FIX_TYPE...
+    release: boolean   // Is it a release point to/from other ATC?
+    altitude?: Level      // Cross altitude
+    mea?: Level           // Minimum enroute altitude (in feet)
+    min_speed?: Speed     // Minimum speed
+    max_speed?: Speed     // Maximum speed
+    freq: string// Navaid frequency
+    visible: boolean    // Default visibility
+    labelVisible: boolean
+    visibleTemp: boolean
+    labelVisibleTemp: boolean
+
+    // Navaid position
+    coordinate: Coordinate
+    x: number
+    y: number
+
+    zoom_min: number  // Min. visible zoom
+    zoom_max: number   // Max. visible zoom
 }
--- src/core/entities/Runway.ts
+++ src/core/entities/Runway.ts
@@ -1,3 +1,33 @@
+import { Bearing, Coordinate } from "../valueObjects";
+
 export interface Runway {
-    
+    // General runway data
+    icao: string
+    name: string         // Runway name
+    label1: string      // Runway indicator on the screen (main direction)
+    label2: string       // Runway indicator on the screen (opposite direction)
+    type: number          // See const RWY_TYPE...
+    heading: Bearing       // Runway main orientation
+    category: number      // See const RWY_CAT...
+    strip_length: number        // Length in feet
+    strip_width: number         // Width in feet
+    runway_visible: boolean
+    centerline_visible: boolean
+
+    // Runway Ends
+    coordinate1: Coordinate
+    coordinate2: Coordinate
+
+    zoom_min: number  // Min. visible zoom
+    zoom_max: number   // Max. visible zoom
+
+    // Runway status
+    active: boolean
+    takeoff: boolean
+    landing: boolean
+
+    // Flight Director Data
+    last_takeoff_time: number
+    last_landing_time: number
+    last_arrival_time: number
 }
